b07a94ca22d1b06fa6899c4709abb211b6cd5819
shubhamraj82 <raj.shubh8216@gmail.com>
2025-12-17 03:04:16 +0530

    all shadcn components are added

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-     Hello World
+     {/* <Header/>
+     <HeroSection/>
+     <FeatureSection/>
+     <PricingSection/>
+     <CtaSection/>
+     <Footer/> */}
     </div>
   );
 }
