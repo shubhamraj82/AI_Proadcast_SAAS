06cc87ea9860fc5171b55c1e8d658c69c48624e7
shubhamraj82 <raj.shubh8216@gmail.com>
2025-12-28 01:27:22 +0530

    done with the dashboard screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+import { Header } from "@/components/Header";
+import { CtaSection } from "@/components/Home/cta-section";
+import { FeaturesSection } from "@/components/Home/features-section";
+import { Footer } from "@/components/Home/footer";
+import { HeroSection } from "@/components/Home/hero-section";
+import { PricingSection } from "@/components/Home/pricing-section";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-     {/* <Header/>
+     <Header/>
      <HeroSection/>
-     <FeatureSection/>
+     <FeaturesSection/>
      <PricingSection/>
      <CtaSection/>
-     <Footer/> */}
+    <Footer/>
     </div>
   );
 }
