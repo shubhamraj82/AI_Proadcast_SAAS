e9c8c0a103a11360c4d616d07819c9932b20121f
shubhamraj82 <raj.shubh8216@gmail.com>
2025-12-30 00:40:29 +0530

    prodacst-uploader done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         {children}
+        <Toaster/>
       </body>
     </html>
     </ConvexClientProvider>
